dc7a3b8e00b54979515088739835ee99da46fc37
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2022-02-02 00:43:46 +0600

    add max and min temp and set previous search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.getElementById("search-btn").addEventListener("click", () => {
   const inputLocation = document.getElementById("input-location").value;
   weatherUpdate(inputLocation);
+  localStorageData(inputLocation);
   localStorage.setItem("prev-search", inputLocation);
 });
 
@@ -17,6 +18,18 @@ function weatherUpdate(city) {
       document.getElementById(
         "image"
       ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+      document.getElementById("main-container").style.border = "2px solid #FFF";
+      document.getElementById("maximum").innerText =
+        "Maximum temperature " + data.main.temp_max;
+      document.getElementById("minimum").innerText =
+        "Minimum temperature " + data.main.temp_min;
     })
     .catch((error) => console.log(error));
+}
+
+function localStorageData(value) {
+  let searchItem = localStorage.getItem("prev-search", value);
+  document.getElementById("title").innerHTML = "Previous searched-";
+  document.getElementById("country").innerHTML = searchItem;
 }
